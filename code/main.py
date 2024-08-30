74eab55c849d405891842950d6177a93d6bd2b1c
realzzz11 <zengqingyu1998@gmail.com>
2024-08-30 14:45:29 +0900

    reset the default of task and dataset

--- code/main.py
+++ code/main.py
@@ -13,8 +13,8 @@ from data_process import training_data_process
 
 
 args = argparse.ArgumentParser(description='Argparse for compound-protein interactions prediction')
-args.add_argument('-task', type=str, default='interaction', help='affinity/interaction')
-args.add_argument('-dataset', type=str, default='human', help='choose a dataset')
+args.add_argument('-task', type=str, default='affinity', help='affinity/interaction')
+args.add_argument('-dataset', type=str, default='Kd', help='choose a dataset')
 args.add_argument('-mode', type=str, default='gpu', help='gpu/cpu')
 args.add_argument('-cuda', type=str, default='0', help='visible cuda devices')
 args.add_argument('-verbose', type=int, default=1, help='0: do not output log in stdout, 1: output log')
